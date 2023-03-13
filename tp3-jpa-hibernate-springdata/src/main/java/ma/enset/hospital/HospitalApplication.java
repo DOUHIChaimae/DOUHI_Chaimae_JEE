f8b1ca9d1cb2be950457a640f0833abf1fe9fdcb
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-03-13 08:57:34 +0100

    adding Consultation,Medecin,RDVStatus entities and repositories

---
+++ tp3-jpa-hibernate-springdata/src/main/java/ma/enset/hospital/HospitalApplication.java
@@ -0,0 +1,33 @@
+package ma.enset.hospital;
+
+import ma.enset.hospital.entities.Patient;
+import ma.enset.hospital.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import java.util.Date;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class HospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(PatientRepository patientRepository) {
+        return args -> {
+            Stream.of("douhi1", "douhi2", "douhi3")
+                    .forEach(name -> {
+                        Patient patient = new Patient();
+                        patient.setName(name);
+                        patient.setBirthday(new Date());
+                        patient.setIsSick(false);
+                        patientRepository.save(patient);
+                    });
+        };
+    }
+
+}
--- tp3-jpa-hibernate-springdata/src/main/java/ma/enset/hospital/entities/Medecin.java
+++ tp3-jpa-hibernate-springdata/src/main/java/ma/enset/hospital/entities/Medecin.java
@@ -4,26 +4,20 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.Collection;
-import java.util.Date;
 
+import java.util.Collection;
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
-public class Patient {
+public class Medecin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "NOM", length = 50)
     private String name;
-    @Temporal(TemporalType.DATE)
-    private Date birthday;
-    private Boolean isSick;
-    private int score;
-    @OneToMany(mappedBy = "patient",fetch = FetchType.LAZY)
+    private String speciality;
+    private String email;
+    @OneToMany(mappedBy = "medecin",fetch = FetchType.LAZY)
     private Collection<RendezVous> rendezVous;
-
 
 }
