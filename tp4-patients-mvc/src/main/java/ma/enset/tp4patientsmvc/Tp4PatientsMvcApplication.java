0fac67d78ed634200e3ae65e334b7c71284f919c
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-03-13 09:26:39 +0100

    adding tp4 project

---
+++ tp4-patients-mvc/src/main/java/ma/enset/tp4patientsmvc/Tp4PatientsMvcApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.tp4patientsmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4PatientsMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4PatientsMvcApplication.class, args);
+	}
+
+}
---
+++ tp4-patients-mvc/src/main/java/ma/enset/tp4patientsmvc/entities/Patient.java
@@ -0,0 +1,26 @@
+package ma.enset.tp4patientsmvc.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "NOM", length = 50)
+    private String name;
+    @Temporal(TemporalType.DATE)
+    private Date birthday;
+    private Boolean isSick;
+    private int score;
+}
