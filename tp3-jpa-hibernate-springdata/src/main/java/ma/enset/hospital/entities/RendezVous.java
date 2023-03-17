a7d809eaee8da7041d4d3988844a0bc34ea55292
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-03-17 09:34:47 +0100

    adding @JsonProperty annotation and implemnting a PatientRestController

--- tp3-jpa-hibernate-springdata/src/main/java/ma/enset/hospital/entities/RendezVous.java
+++ tp3-jpa-hibernate-springdata/src/main/java/ma/enset/hospital/entities/RendezVous.java
@@ -1,5 +1,6 @@
 package ma.enset.hospital.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,15 +13,15 @@ import java.util.Date;
 @AllArgsConstructor
 public class RendezVous {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private Date date;
     @Enumerated(EnumType.STRING)
     private RDVStatus status;
     @ManyToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Patient patient;
     @ManyToOne
     private Medecin medecin;
     @OneToOne(mappedBy = "rendezVous")
-    Consultation consultation;
+    private Consultation consultation;
 }
