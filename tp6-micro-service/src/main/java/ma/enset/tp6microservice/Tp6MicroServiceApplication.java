e8c005e9e554a109536e8169e2e74a465bb65a76
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-04-25 20:10:40 +0100

    initialize tp6-micro-service

---
+++ tp6-micro-service/src/main/java/ma/enset/tp6microservice/Tp6MicroServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.tp6microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp6MicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp6MicroServiceApplication.class, args);
+	}
+
+}
---
+++ tp6-micro-service/src/test/java/ma/enset/tp6microservice/Tp6MicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.tp6microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp6MicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
