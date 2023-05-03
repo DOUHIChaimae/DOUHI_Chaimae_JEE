6df0ef632903429c6e8f60d54f1c3fd5232c2ab3
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-05-03 18:46:44 +0100

    Adding CustomDataFetcherExceptionResolver class and updating the schema graphql file

--- tp6-bank-account-service/src/main/java/ma/enset/tp6bankaccountservice/web/BankAccountGraphQLController.java
+++ tp6-bank-account-service/src/main/java/ma/enset/tp6bankaccountservice/web/BankAccountGraphQLController.java
@@ -1,8 +1,16 @@
 package ma.enset.tp6bankaccountservice.web;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ma.enset.tp6bankaccountservice.dtos.BankAccountRequestDTO;
+import ma.enset.tp6bankaccountservice.dtos.BankAccountResponseDto;
 import ma.enset.tp6bankaccountservice.entities.BankAccount;
 import ma.enset.tp6bankaccountservice.repositories.BankAccountRepository;
+import ma.enset.tp6bankaccountservice.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -12,9 +20,27 @@ import java.util.List;
 public class BankAccountGraphQLController {
     @Autowired
     private BankAccountRepository bankAccountRepository;
+    @Autowired
+    private AccountService accountService;
 
     @QueryMapping
     public List<BankAccount> accountsList() {
         return bankAccountRepository.findAll();
     }
+
+    @QueryMapping
+    public BankAccount bankAccountById(@Argument String id) {
+        return bankAccountRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(String.format("Account %s not found", id)));
+    }
+
+    @MutationMapping
+    public BankAccountResponseDto addAccount(@Argument BankAccountRequestDTO bankAccount) {
+        return accountService.addAccount(bankAccount);
+    }
 }
+/*
+record BankAccountDTO{
+*/
+
+
