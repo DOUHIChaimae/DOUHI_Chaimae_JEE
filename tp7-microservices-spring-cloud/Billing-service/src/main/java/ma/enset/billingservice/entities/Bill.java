b858f8f5984bcc4249c9a10fd64be7e2e586cd63
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-06-01 15:17:35 +0100

    adding billing-service

---
+++ tp7-microservices-spring-cloud/Billing-service/src/main/java/ma/enset/billingservice/entities/Bill.java
@@ -0,0 +1,20 @@
+package ma.enset.billingservice.entities;
+
+import jakarta.persistence.*;
+import ma.enset.billingservice.model.Customer;
+
+import java.util.Collection;
+import java.util.Date;
+@Entity
+
+public class Bill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private Date billingDate;
+    @OneToMany(mappedBy = "bill")
+    private Collection<ProductItem> productItems;
+    private long customerID;
+    @Transient
+    private Customer customer;
+}
