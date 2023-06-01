be4cc5f2b3e1ed5de81c3d6255ff8ad507990f76
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-06-01 16:16:06 +0100

    adding BillingRestController

---
+++ tp7-microservices-spring-cloud/Billing-service/src/main/java/ma/enset/billingservice/web/BillingRestController.java
@@ -0,0 +1,29 @@
+package ma.enset.billingservice.web;
+
+import ma.enset.billingservice.entities.Bill;
+import ma.enset.billingservice.feign.CustomerRestClient;
+import ma.enset.billingservice.feign.ProductItemRestClient;
+import ma.enset.billingservice.repositories.BillRepository;
+import ma.enset.billingservice.repositories.ProductItemRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class BillingRestController {
+    private BillRepository billRepository;
+    private ProductItemRepository productItemRepository;
+    private CustomerRestClient customerRestClient;
+    private ProductItemRestClient productItemRestClient;
+
+    public BillingRestController(BillRepository billRepository, ProductItemRepository productItemRepository, CustomerRestClient customerRestClient, ProductItemRestClient productItemRestClient) {
+        this.billRepository = billRepository;
+        this.productItemRepository = productItemRepository;
+        this.customerRestClient = customerRestClient;
+        this.productItemRestClient = productItemRestClient;
+    }
+
+    @GetMapping(path = "/fullBill/{id}")
+    public Bill getBill(@PathVariable Long id) {
+        Bill bill = billRepository.findById(id).get();
+        return bill;
+    }
+}
